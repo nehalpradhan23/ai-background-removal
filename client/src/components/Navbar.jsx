fa624d9d1baeac054eb1911c1291001dbcf5b3c0
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2025-01-18 20:33:46 +0530

    clerk setup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser();
+  // ==================================================
   return (
     <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
       <Link to={"/"}>
@@ -13,10 +17,21 @@ const Navbar = () => {
         </span>
         {/* </div> */}
       </Link>
-      <button className="flex transition-all bg-blue-700 hover:shadow-xl hover:bg-blue-800 text-white items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 rounded-md">
-        Get started
-        <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
-      </button>
+      {/* if signed in---------------------------------------- */}
+      {isSignedIn ? (
+        <div className="">
+          <UserButton />
+        </div>
+      ) : (
+        // {/* sign in button ======================= */}
+        <button
+          onClick={() => openSignIn({})}
+          className="flex transition-all bg-blue-700 hover:shadow-xl hover:bg-blue-800 text-white items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 rounded-md"
+        >
+          Get started
+          <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+        </button>
+      )}
     </div>
   );
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./"), // Adjust to match your project structure
-    },
-  },
 });
